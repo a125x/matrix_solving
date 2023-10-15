5855e82e6e8c4626573c7c41f4755f493c8cffde
a125x <alexander.andr.shat@proton.me>
2023-10-15 16:55:41 +0300

    fixed typos

--- evm1.cpp
+++ evm1.cpp
@@ -3,7 +3,7 @@
 #include "matrix_solver.h"
 #include "matrix_reader.h"
 
-//function to create matrix 
+//function to create element of matrix 
 double f(int i, int j, int n, int mode)
 {
     switch (mode)
@@ -23,6 +23,7 @@ double f(int i, int j, int n, int mode)
     return 0.;
 }
 
+//initializing matrix A
 void inint_A_form(double *A, int n, int k)
 {
     for (int i = 0; i < n; i++)
@@ -34,6 +35,7 @@ void inint_A_form(double *A, int n, int k)
     }
 }
 
+//initializing vector B
 void init_B(double *A, double *B, int n)
 {
     double temp = 0;
@@ -51,6 +53,7 @@ void init_B(double *A, double *B, int n)
     }
 }
 
+//printing any matrix
 void print_mat(double *A, int n, int m, int p)
 {
     for (int i = 0; i < n && i < p; i++)
@@ -64,6 +67,7 @@ void print_mat(double *A, int n, int m, int p)
     }
 }
 
+//calculating resid
 double res_ctr(double *A, double *B, double *X, int n)
 {
     double sum = 0.;
@@ -78,8 +82,8 @@ double res_ctr(double *A, double *B, double *X, int n)
             sum_str += A[i * n + j] * X[j];
         }
         
-        sum += f_abs (sum_str - B[i]);
-        norm_b += f_abs (B[i]);
+        sum += f_abs(sum_str - B[i]);
+        norm_b += f_abs(B[i]);
     }
     
     return (norm_b > EPS ? sum / norm_b : 0);
@@ -93,13 +97,13 @@ int main(int argc, char *argv[])
 
     //n is matrix dimentions, m is amount of outputs of matrix,
     //k is a formula number
-    int n = 0, k = 0, m = 0, task = 11;
+    int n = 0, k = 0, m = 0;
 
     //checking if input data are correct
     if (!((argc == 4 || argc == 5) 
-        && (sscanf (argv[1], "%d", &n) == 1) && (n > 0)
-        && (sscanf (argv[2], "%d", &m) == 1)
-        && (sscanf (argv[3], "%d", &k) == 1)
+        && (sscanf(argv[1], "%d", &n) == 1) && (n > 0)
+        && (sscanf(argv[2], "%d", &m) == 1)
+        && (sscanf(argv[3], "%d", &k) == 1)
         && ((k == 0 && argc == 5) || (k > 0 && k <= 4 && argc == 4))))
     {
         printf ("Usage: %s n m k [file]\n", argv[0]);
@@ -122,7 +126,7 @@ int main(int argc, char *argv[])
         return 0;
     }
     
-    double t1 = 0., t2 = 0.;
+    double t1 = 0.;
     int res = 0;
     char *file = nullptr;
     
@@ -208,11 +212,12 @@ int main(int argc, char *argv[])
     init_B(A, B, n);
 
     double resid = 0.;
-    t2 = clock();
+    //t2 = clock();
     resid = res_ctr(A, B, X, n);
-    t2 = (clock() - t2) / CLOCKS_PER_SEC;
+    //t2 = (clock() - t2) / CLOCKS_PER_SEC;
  
-    printf ("%s : Task = %d Res = %e T1 = %.2f T2 = %.2f K = %d N = %d M = %d\n", argv[0], task, resid, t1, t2, k, n, m);
+    //printf ("%s : Res = %e T1 = %.2f T2 = %.2f K = %d N = %d M = %d\n", argv[0], resid, t1, t2, k, n, m);
+    printf ("%s : Res = %e T1 = %.2f K = %d N = %d M = %d\n", argv[0], resid, t1, k, n, m);
 
     delete [] A;
     delete [] B;
--- matrix_reader.cpp
+++ matrix_reader.cpp
@@ -5,14 +5,13 @@
 int inint_A_file(double *A, int n, char *file)
 {
     FILE *in = fopen(file, "r");
-    int i = 0, j = 0;
 
     if (!in)
       return 1;
 
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
         {
             if (fscanf(in, "%lf", A + i * n + j) != 1)
             {
