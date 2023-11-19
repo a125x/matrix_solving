0eb9d7d359ce75f0d0ba61e28176930b4eeb067a
a125x <alexander.andr.shat@proton.me>
2023-11-20 00:06:31 +0300

    commented entire matrix_solve algorithm and started creating multythreaded version

--- matrix_solver.cpp
+++ matrix_solver.cpp
@@ -1,10 +1,13 @@
 #include "matrix_solver.h"
+#include <thread>
+#include <mutex>
 
 double f_abs(double a)
 {
     return (a > 0 ? a : -a);
 }
 
+//finding the solution of the given triangular matrix
 void reverse_stroke(double *A, double *B, double *X, int n)
 {
     double src = 0.;
@@ -20,27 +23,98 @@ void reverse_stroke(double *A, double *B, double *X, int n)
     }
 }
 
-//solving matrix a with values b and writing results to x
-int solve(double *A, double *B, double *X, int n)
+int th_solve(double *A, double *B, double *X, int n, int ind_max, double mnoz, double maxx, int steps)
+
+{
+    ind_max = steps;
+    maxx = f_abs(A[steps * n + steps]);
+
+    for (int i = steps; i < n; i++)
+        if (f_abs(A[steps + i * n]) > maxx)
+        {
+            maxx = f_abs(A[steps + i * n]);
+            ind_max = i;
+        }
+
+    if (maxx < EPS)
+        return -1;
+
+    if (ind_max != steps)
+    {
+        double tmp = B[steps];
+        B[steps] = B[ind_max];
+        B[ind_max] = tmp;
+        for (int j = steps; j < n; j++)
+        {
+            tmp = A[ind_max * n + j];
+            A[ind_max * n + j] = A[steps * n + j];
+            A[steps * n + j] = tmp;
+        }
+    }
+ 
+    mnoz = 1. / A[steps * n + steps];
+    B[steps] *= mnoz;
+    
+    for (int j = steps; j < n; j++)
+        A[steps * n + j] *= mnoz;
+    
+    for (int i = steps + 1; i < n; i++)
+    {
+        mnoz = A[i * n + steps];
+        for (int j = steps; j < n; j++)
+            A[i * n + j] -= mnoz * A[steps * n + j];
+        
+        B[i] -= mnoz * B[steps];
+    }
+
+    return 0;
+}
+
+//preparing matrix to solve by making it triangular
+int new_solve(double *A, double *B, double *X, int n)
 {
     int ind_max = 0;
     double mnoz = 0., maxx = 0.;
 
     for (int steps = 0; steps < n; steps++)
     {
+    
+    }
+    
+    reverse_stroke (A, B, X, n);
+    
+    return 0;
+}
+
+//preparing matrix to solve by making it triangular
+int solve(double *A, double *B, double *X, int n)
+{
+    //maxx is a maximal element in the column
+    //and ind_max is its index
+    int ind_max = 0;
+    double mnoz = 0., maxx = 0.;
+
+    for (int steps = 0; steps < n; steps++)
+    {
+        //choosing maximal element and finding its index
         ind_max = steps;
         maxx = f_abs(A[steps * n + steps]);
 
         for (int i = steps; i < n; i++)
+            //if elem > max it becomes max and changing 
+            //index correspondingly
             if (f_abs(A[steps + i * n]) > maxx)
             {
                 maxx = f_abs(A[steps + i * n]);
                 ind_max = i;
             }
 
+        //if maximal element is lower than constant return error
         if (maxx < EPS)
             return -1;
 
+        //if choosen max element is not already what we're 
+        //working with, changing its position to the current one
         if (ind_max != steps)
         {
             double tmp = B[steps];
@@ -53,13 +127,21 @@ int solve(double *A, double *B, double *X, int n)
                 A[steps * n + j] = tmp;
             }
         }
-     
+    
+        //mnoz is equal to the element on the diagonal on the
+        //steps row to the power of -1 and multiplying matrix
+        //row and the answer to it creating nice 1-equal 
+        //diagonal
         mnoz = 1. / A[steps * n + steps];
         B[steps] *= mnoz;
         
         for (int j = steps; j < n; j++)
             A[steps * n + j] *= mnoz;
-        
+       
+        //removing all the column with this Xsteps below by
+        //subtracting steps row from all the rows below 
+        //mnoz times (mnoz becomes equal to Xsteps i for all
+        //the is below Xsteps i'm so tired rn fuck
         for (int i = steps + 1; i < n; i++)
         {
             mnoz = A[i * n + steps];
@@ -70,6 +152,7 @@ int solve(double *A, double *B, double *X, int n)
         }
     }
     
+    //solving prepared matrix
     reverse_stroke (A, B, X, n);
     
     return 0;
